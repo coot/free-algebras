99822158796b010582d75b89e2fd0641267a82b8
Marcin Szamotulski <profunctor@pm.me>
2018-08-02 19:40:50 +0100

    Added PointedMonoid newtype wrapper

--- src/Control/Monad/Action.hs
+++ src/Control/Monad/Action.hs
@@ -6,17 +6,31 @@ import           Control.Monad (join)
 import           Data.Functor.Const (Const (..))
 
 import           Control.Algebra.Free (AlgebraType0, AlgebraType, FreeAlgebra1 (..))
+import           Data.Algebra.Pointed (Pointed (point))
 import           Data.Algebra.Free (FreeAlgebra, foldFree)
 
 -- |
 -- A monad action is an `m`-algebra parametrized over a functor `f`.
 -- This is direct translation of a monoid action in the monoidal category of
 -- endofunctors with monoidal product: functor composition.
+--
+-- @'mact'@ should be associative:
+-- prop> 'mact' . 'mact' = 'mact' . 'join'
+-- and unital:
+-- prop> mact . return = id
+--
+-- There are monads which do not have any (safe) instances, like @'IO'@.
 class (Monad m, Functor f) => MAction m f where
     mact :: m (f a) -> f a
 
 instance Monad m => MAction m m where
     mact = join
+
+-- |
+-- You can use @'PointedMonoid'@ newtype wrapper if you want to laverage
+-- @'Pointed'@ instance for a @'Monoid'@.
+instance (Pointed r, Functor f) => MAction ((->) r) f where
+    mact f = f point
 
 -- |
 -- Every algebra @d@ which satisfies the constraint @'AlgebraType' m d@ lifts
@@ -26,7 +40,7 @@ instance (Monad m, FreeAlgebra m, AlgebraType m d) => MAction m (Const d) where
     mact mca = Const $ foldFree $ getConst <$> mca
 
 newtype FreeMAction m f a = FreeMAction { runFreeMAction :: m (f a) }
-    deriving (Functor)
+    deriving (Show, Eq, Ord, Functor)
 
 instance (Monad m, Functor f) => MAction m (FreeMAction m f) where
     mact mfa = FreeMAction $ join $ runFreeMAction <$> mfa
--- src/Data/Algebra/Pointed.hs
+++ src/Data/Algebra/Pointed.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE UndecidableInstances #-}
 module Data.Algebra.Pointed
     ( Pointed (..)
+    , PointedMonoid (..)
     ) where
 
 
@@ -12,5 +13,16 @@ class Pointed p where
 instance Pointed (Maybe a) where
     point = Nothing
 
-instance Monoid m => Pointed m where
+-- |
+-- @Monoid@ should be a subclass of @Pointed@.
+newtype PointedMonoid m = PointedMonoid { runPointedMonoid :: m }
+    deriving (Show, Eq, Ord, Functor)
+
+instance Semigroup m => Semigroup (PointedMonoid m) where
+    (PointedMonoid m) <> (PointedMonoid n) = PointedMonoid (m <> n)
+
+instance Monoid m => Monoid (PointedMonoid m) where
+    mempty = PointedMonoid mempty
+
+instance Monoid m => Pointed (PointedMonoid m) where
     point = mempty
