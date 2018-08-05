1368f6006952dbcd473e51a1755dcd6d7ee32fff
Marcin Szamotulski <profunctor@pm.me>
2018-08-05 23:35:42 +0100

    Monadicity: MAlg instances, remove FreeAlg

--- example/src/Data/Algebra/Free/Monadicity.hs
+++ example/src/Data/Algebra/Free/Monadicity.hs
@@ -1,10 +1,43 @@
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE PolyKinds #-}
-module Data.Algebra.Free.Monadicity where
+module Data.Algebra.Free.Monadicity
+    ( Hom (..)
+    , unHom
+    , idHom
+    , composeHom
+    , bimapHom
+    , AlgHom (..)
+    , unAlgHom
+    , idAlgHom
+    , composeAlgHom
+    , bimapAlgHom
+    , forget
+    , right
+    , left
+    , unit
+    , counit
+    , FreeMAlg (..)
+    , runFreeMAlg
+    , fmapF
+    , returnF
+    , joinF
+    , bindF
+    , MAlg (..)
+    , algfn
+    , algFreeMAlg
+    , foldMapFreeMAlg
+    , foldFreeMAlg
+    , k
+    , k_inv_monoid_mempty
+    , k_inv_monoid_mappend
+    , k_inv_semigroup_append
+    , k_inv_pointed
+    , k_inv_group_invert
+    , k_inv_act
+    ) where
 
 import           Prelude
 
-import           Control.Monad (join)
+import           Data.Bifunctor (bimap)
 import           Data.Constraint (Dict (..))
 import           Data.List.NonEmpty (NonEmpty (..))
 import qualified Data.List.NonEmpty as NE
@@ -23,13 +56,13 @@ data Hom m a b where
 unHom :: Hom m a b -> a -> b
 unHom (Hom f) = f
 
+idHom :: AlgebraType0 m a => Hom m a a
+idHom = Hom id
+
 -- |
 -- @'Hom'@ is a category.
 composeHom :: Hom m b c -> Hom m a b -> Hom m a c
 composeHom (Hom f) (Hom g) = Hom (f . g)
-
-idHom :: AlgebraType0 m a => Hom m a a
-idHom = Hom id
 
 bimapHom :: forall m a' a b b'.
             ( AlgebraType0 m a'
@@ -58,13 +91,13 @@ unAlgHom (AlgHom f) = f
 forget :: AlgHom m a b -> Hom m a b
 forget (AlgHom f) = Hom f
 
+idAlgHom :: (AlgebraType0 m a, AlgebraType m a) => AlgHom m a a
+idAlgHom = AlgHom id
+
 -- |
 -- @'AlgHom'@ is a category
 composeAlgHom :: AlgHom m b c -> AlgHom m a b -> AlgHom m a c
 composeAlgHom (AlgHom f) (AlgHom g) = AlgHom (f . g)
-
-idAlgHom :: (AlgebraType0 m a, AlgebraType m a) => AlgHom m a a
-idAlgHom = AlgHom id
 
 bimapAlgHom :: forall m a' a b b'.
                ( AlgebraType0 m a'
@@ -129,12 +162,29 @@ counit :: forall m d .
 counit = left (Hom id)
 
 -- |
--- The monad associated with the adjunction.  Note that it's isomorphic to @m@
+-- The monad associated with the adjunction.  Note that it's isomorphic to @'FreeAlgebra' m => m@.
 data FreeMAlg (m :: * -> *) (a :: *) where
     FreeMAlg :: FreeAlgebra m => m a -> FreeMAlg m a
 
+instance Show (m a) => Show (FreeMAlg m a) where
+    show (FreeMAlg ma) = "FreeMAlg " ++ show ma
+
+instance Eq (m a) => Eq (FreeMAlg m a) where
+    (FreeMAlg ma) == (FreeMAlg mb) = ma == mb
+
+instance Ord (m a) => Ord (FreeMAlg m a) where
+    compare (FreeMAlg ma) (FreeMAlg mb) = ma `compare` mb
+
 runFreeMAlg :: FreeMAlg m a -> m a
 runFreeMAlg (FreeMAlg ma) = ma
+
+-- |
+-- @'FreeMAlg'@ is a functor in the category @Hom m@.
+fmapF :: forall m a b .
+         Hom m a b
+      -> FreeMAlg m a
+      -> FreeMAlg m b
+fmapF (Hom fn) (FreeMAlg ma) = FreeMAlg $ fmapFree fn ma
 
 -- |
 -- unit of the @FreeMAlg@ monad (i.e. @return@ in Haskell)
@@ -176,55 +226,84 @@ bindF (FreeMAlg ma) (Hom f) = FreeMAlg $ ma `bindFree` (runFreeMAlg . f)
 class MAlg m a where
     alg :: m a -> a
 
-instance (Monad m, FreeAlgebra m) => MAlg m (FreeMAlg m a) where
-    alg ma = FreeMAlg $ join $ fmap runFreeMAlg ma
+instance {-# OVERLAPPING #-} MAlg [] [a] where
+    alg = concat
+
+instance {-# OVERLAPPING #-} MAlg NonEmpty (NonEmpty a) where
+    alg = NE.fromList . concatMap NE.toList . NE.toList
 
 -- |
--- @FreeAlg@ is an instance of @FreeAlgebra@ with @returnFreeAlg@ and
+-- Wrapping an @MAlg@ inside a monad is a 
+instance {-# OVERLAPPABLE #-} (Functor m, MAlg m a, MAlg m b) => MAlg m (a, b) where
+    alg = bimap alg alg . NE.unzip
+
+-- |
+-- TODO: is this a lawful instance?
+instance {-# OVERLAPPABLE #-} (Traversable m, Monad n, MAlg m a) => MAlg m (n a) where
+    alg mna = alg <$> sequence mna
+
+-- |
+-- if @MAlg m a@ holds then @MAlg m (b -> a)@ holds
+algfn :: ( FreeAlgebra  m
+         , AlgebraType0 m (b -> a)
+         , AlgebraType0 m a
+         , MAlg m a
+         )
+      => m (b -> a)
+      -> (b -> a)
+algfn mbtoa b = alg $ fmapFree ($ b) mbtoa
+
+instance MAlg [] a => MAlg [] (b -> a) where
+    alg = algfn
+
+instance MAlg NonEmpty a => MAlg NonEmpty (b -> a) where
+    alg = algfn
+
+-- $FreeMAlg-FreeAlgebra
+--
+-- @FreeMAlg@ is an instance of @FreeAlgebra@ with @returnFreeMAlg@ and
 -- @foldMapFreeMalg@.  The only problem with typing this instance is that ghc
 -- will not be able to deduce @AlgebraType0 m a@ instance even if we define
 -- @
---    type instance AlgebraType0 (FreeAlg m) a = AlgebraType0 m a
+--    type instance AlgebraType0 (FreeMAlg m) a = AlgebraType0 m a
 -- @
-data FreeAlg m a where
-    FreeAlg :: ( FreeAlgebra m, AlgebraType0 m a ) => m a -> FreeAlg m a
 
-runFreeAlg :: FreeAlg m a -> m a
-runFreeAlg (FreeAlg ma) = ma
-
+-- This is undecidable instance:
 {--
   - instance ( FreeAlgebra m
   -          , AlgebraType0 m a
   -          , AlgebraType0 m (m a)
-  -          , AlgebraType0 m (FreeAlg m a)
-  -          )
-  -          => MAlg m (FreeAlg m a) where
-  -     alg mma = FreeAlg $ joinFree $ fmapFree runFreeAlg mma
+  -          , AlgebraType0 m (FreeMAlg m a)
+  -          ) => MAlg m (FreeMAlg m a) where
+  -     alg ma = FreeMAlg $ joinFree $ fmapFree runFreeMAlg ma
   --}
 
-returnFreeAlg
-    :: ( FreeAlgebra m
+-- So let's just capture it in a function.
+algFreeMAlg
+    :: ( FreeAlgebra  m
        , AlgebraType0 m a
+       , AlgebraType0 m (m a)
+       , AlgebraType0 m (FreeMAlg m a)
        )
-    => a
-    -> FreeAlg m a
-returnFreeAlg = FreeAlg . returnFree
+    => m (FreeMAlg m a)
+    -> FreeMAlg m a
+algFreeMAlg ma = FreeMAlg $ joinFree $ fmapFree runFreeMAlg ma
 
-foldMapFreeAlg
+foldMapFreeMAlg
     :: ( AlgebraType0 m a
        , AlgebraType0 m d
        , MAlg m d
        )
     => (a -> d)
-    -> (FreeAlg m a -> d)
-foldMapFreeAlg fn (FreeAlg ma) = alg $ fmapFree fn ma
+    -> (FreeMAlg m a -> d)
+foldMapFreeMAlg fn (FreeMAlg ma) = alg $ fmapFree fn ma
 
-foldFreeAlg
+foldFreeMAlg
     :: ( AlgebraType0 m a
        , MAlg m a
        ) 
-    => FreeAlg m a -> a
-foldFreeAlg = foldMapFreeAlg id
+    => FreeMAlg m a -> a
+foldFreeMAlg = foldMapFreeMAlg id
 
 -- |
 -- The comparison functor from the category of algebras of type @AgelbraType
@@ -242,49 +321,29 @@ k :: ( FreeAlgebra  m
 k _ = foldFree
 
 -- |
--- As above but as an instance (commented out, since its undecidable instance
--- only because the @AlgebraType m a@ is no smaller than the instance head).
-{--
-  - instance ( FreeAlgebra  m
-  -          , AlgebraType  m a
-  -          , AlgebraType0 m a
-  -          )
-  -          => MAlg m a where
-  -     alg = k Proxy
-  --}
-
--- |
 -- @'mempty'@ deduced from @FreeAlg []@ @MAlg@ instance.
 k_inv_monoid_mempty :: MAlg [] a => a
-k_inv_monoid_mempty = foldFreeAlg (FreeAlg [])
+k_inv_monoid_mempty = foldFreeMAlg (FreeMAlg [])
 
 -- |
 -- @'mappend'@ deduced from @FreeAlg []@ @MAlg@ instance.
 k_inv_monoid_mappend :: MAlg [] a => a -> a -> a
-k_inv_monoid_mappend a b = foldFreeAlg (FreeAlg [a, b])
+k_inv_monoid_mappend a b = foldFreeMAlg (FreeMAlg [a, b])
 
 -- |
 -- @'<>'@ deduced from @FreeAlg NonEmpty@ @MAlg@ instance.
 k_inv_semigroup_append :: MAlg NonEmpty a => a -> a -> a
-k_inv_semigroup_append a b = foldFreeAlg (FreeAlg (a :| [b]))
+k_inv_semigroup_append a b = foldFreeMAlg (FreeMAlg (a :| [b]))
 
 k_inv_pointed :: MAlg Maybe a => a
-k_inv_pointed = foldFreeAlg (FreeAlg Nothing)
+k_inv_pointed = foldFreeMAlg (FreeMAlg Nothing)
 
 -- |
 -- @'invert'@ deduced from @FreeAlg FreeGroup@ @MAlg@ instance.
 k_inv_group_invert :: (MAlg FreeGroup a, Eq a) => a -> a
-k_inv_group_invert a = foldFreeAlg (FreeAlg (FreeGroup.fromList [Left a]))
+k_inv_group_invert a = foldFreeMAlg (FreeMAlg (FreeGroup.fromList [Left a]))
 
 -- |
 -- @'act'@ deduced from @FreeAlg FreeMSet@ @MAlg@ instance.
 k_inv_act :: (MAlg (FreeMSet m) a, Monoid m) => m -> a -> a
-k_inv_act m a = foldFreeAlg $ FreeAlg $ FreeMSet (m, a)
-
--- Some instances to play with
-
-instance MAlg [] [a] where
-    alg = concat
-
-instance MAlg NonEmpty (NonEmpty a) where
-    alg = NE.fromList . concat . fmap NE.toList . NE.toList
+k_inv_act m a = foldFreeMAlg $ FreeMAlg $ FreeMSet (m, a)
