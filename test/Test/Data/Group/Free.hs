320feb3e8b0b36a6b4c7a729b0215d402582d780
Marcin Szamotulski <profunctor@pm.me>
2019-01-06 23:41:39 +0100

    Improved test for Data.Group.Free

--- test/Test/Data/Group/Free.hs
+++ test/Test/Data/Group/Free.hs
@@ -3,8 +3,8 @@ module Test.Data.Group.Free
     ( tests
     ) where
 
-import           Control.Monad (mapM)
 import           Data.Semigroup (Semigroup (..))
+import           Data.Bool (bool)
 import           Data.Group (invert)
 import           Data.DList (DList)
 import qualified Data.DList as DList
@@ -13,19 +13,16 @@ import qualified Hedgehog as H
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 
-import           Data.Group.Free (fromDList, normalize)
+import           Data.Group.Free (fromDList, normalize, fromList, normalizeL)
+
+genList :: Gen a -> Gen [Either a a]
+genList gen =
+    Gen.list (Range.linear 0 100) gen'
+  where
+    gen' = Gen.bool >>= bool (Right <$> gen) (Left <$> gen)
 
 genDList :: Gen a -> Gen (DList (Either a a))
-genDList gen = do
-    as <- Gen.list (Range.linear 0 100) gen
-    DList.fromList <$> mapM
-        (\a -> do
-            b <- Gen.bool
-            if b
-            then return $ Right a
-            else return $ Left a
-        )
-        as
+genDList gen = DList.fromList <$> genList gen
 
 prop_normalize :: Property
 prop_normalize = property $ do
@@ -33,7 +30,7 @@ prop_normalize = property $ do
 
     normalize (normalize as) === normalize as
     normalize (as `DList.append` rev as) === DList.empty
-    where
+  where
     rev :: DList (Either a a) -> DList (Either a a)
     rev = DList.foldr (\a as -> DList.snoc as (either Right Left a)) DList.empty
 
@@ -49,14 +46,47 @@ prop_unit :: Property
 prop_unit = property $ do
     fg <- fromDList <$> H.forAll (genDList Gen.bool)
 
-    fg <> mempty       === fg
-    mempty <> fg       === fg
+    fg <> mempty === fg
+    mempty <> fg === fg
 
 prop_associativity :: Property
 prop_associativity = property $ do
     fg   <- fromDList <$> H.forAll (genDList Gen.bool)
     fg'  <- fromDList <$> H.forAll (genDList Gen.bool)
     fg'' <- fromDList <$> H.forAll (genDList Gen.bool)
+
+    (fg <> fg') <> fg'' === fg <> (fg' <> fg'')
+
+prop_normalizeL :: Property
+prop_normalizeL = property $ do
+    as <- H.forAll (genList Gen.bool)
+
+    normalizeL (normalizeL as) === normalizeL as
+    normalizeL (as ++ rev as) === []
+  where
+    rev :: [Either a a] -> [Either a a]
+    rev = foldl (\as a -> either Right Left a : as) []
+
+prop_invertL :: Property
+prop_invertL = property $ do
+    fg <- fromList <$> H.forAll (genList Gen.bool)
+
+    invert (invert fg) === fg
+    invert fg <> fg    === mempty
+    fg <> invert fg    === mempty
+
+prop_unitL :: Property
+prop_unitL = property $ do
+    fg <- fromList <$> H.forAll (genList Gen.bool)
+
+    fg <> mempty === fg
+    mempty <> fg === fg
+
+prop_associativityL :: Property
+prop_associativityL = property $ do
+    fg   <- fromList <$> H.forAll (genList Gen.bool)
+    fg'  <- fromList <$> H.forAll (genList Gen.bool)
+    fg'' <- fromList <$> H.forAll (genList Gen.bool)
 
     (fg <> fg') <> fg'' === fg <> (fg' <> fg'')
 
