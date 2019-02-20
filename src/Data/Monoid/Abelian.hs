e684cfc9b01c1c1dc3eb56dafdbef3ac845392c9
Marcin Szamotulski <profunctor@pm.me>
2019-02-20 23:21:21 +0100

    Data.Monoid.Abelian: expanded documentation

--- src/Data/Monoid/Abelian.hs
+++ src/Data/Monoid/Abelian.hs
@@ -16,11 +16,19 @@ import           Data.Semigroup.Abelian (AbelianSemigroup)
 -- |
 -- Free abelian monoid.  Note that `FreeAbelianMonoid () ≅ Natural` as
 -- expected.
-newtype FreeAbelianMonoid a = FreeAbelianMonoid (Map a Natural)
+--
+-- It is a monad on the full subcategory which satisfies the `Ord` constraint,
+-- but base does not allow to define a functor \/ applicative \/ monad
+-- instances which are constraint by a class.
+--
+newtype FreeAbelianMonoid a = FreeAbelianMonoid {
+        runFreeAbelianMonoid :: Map a Natural
+    }
     deriving (Eq, Ord, Show)
 
 instance Ord a => Semigroup (FreeAbelianMonoid a) where
-    (FreeAbelianMonoid a) <> (FreeAbelianMonoid b) = FreeAbelianMonoid $ Map.unionWith (+) a b
+    FreeAbelianMonoid a <> FreeAbelianMonoid b =
+        FreeAbelianMonoid $ Map.unionWith (+) a b
 
 instance Ord a => AbelianSemigroup (FreeAbelianMonoid a)
 
@@ -32,8 +40,12 @@ instance Ord a => Monoid (FreeAbelianMonoid a) where
 
 type instance AlgebraType0 FreeAbelianMonoid a = Ord a
 type instance AlgebraType  FreeAbelianMonoid m = (Ord m, Monoid m, AbelianSemigroup m)
+
 instance FreeAlgebra FreeAbelianMonoid where
     returnFree a = FreeAbelianMonoid (Map.singleton a 1)
-    foldMapFree g (FreeAbelianMonoid as) = Map.foldMapWithKey (\a n -> stimes n $ g a) as 
-    codom  = proof
-    forget = proof
+
+    foldMapFree g (FreeAbelianMonoid as)
+                 = Map.foldMapWithKey (\a n -> stimes n $ g a) as
+
+    codom        = proof
+    forget       = proof
