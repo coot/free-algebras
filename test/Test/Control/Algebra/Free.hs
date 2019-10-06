3e712d752da6afee51e8a30959ab75415c8c8394
Marcin Szamotulski <profunctor@pm.me>
2019-10-06 15:37:46 +0100

    foldFreeNat: document and test properties
    -- prop> foldNatFree nat (liftFree m) = nat m
    -- prop> foldNatFree nat . foldNatFree nat' = foldNatFree (foldNatFree nat . nat')
    
    Test for 'Coyoneda', 'Ap' and 'Free'.

--- test/Test/Control/Algebra/Free.hs
+++ test/Test/Control/Algebra/Free.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE KindSignatures      #-}
 {-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell     #-}
@@ -12,17 +13,22 @@ import           Control.Applicative.Free (Ap)
 import qualified Control.Applicative.Free as Ap
 import           Control.Monad.Free (Free)
 import qualified Control.Monad.Free as Free
+import qualified Control.Monad.State.Strict as S
+import           Data.Functor (($>))
 import           Data.Functor.Identity (Identity (..))
 import           Data.Functor.Coyoneda (Coyoneda (..), lowerCoyoneda)
 import           Hedgehog (Property, PropertyT, Gen, property, (===))
 import qualified Hedgehog as H
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
+import           Text.Show.Functions ()
 
 import           Data.Algebra.Free ( AlgebraType )
+import           Data.Proxy (Proxy (..))
 import           Control.Algebra.Free
                     ( AlgebraType0
                     , FreeAlgebra1 (..)
+                    , Proof (..)
                     , unFoldNatFree
                     , foldFree1
                     , hoistFree1
@@ -205,7 +211,7 @@ hoistFree1_property
     => Gen (m f a)
     -> (m f a -> String)
     -> (m g a -> m g a -> Bool)
-    -> (forall  x.   f x -> g x)
+    -> (forall  x. f x -> g x)
     -> ((forall x. f x -> g x) -> m f a -> m g a)
     -- ^ reference hoist impelentation
     -> Property
@@ -277,6 +283,143 @@ prop_iterFree1_ap = iterFree1_property
     (show . Ap.retractAp)
     runIdentity
     Ap.iterAp
+
+foldNatFree_liftFree_property
+    :: forall m g f a proxy n.
+       ( FreeAlgebra1 m
+       , AlgebraType0 m f
+       , AlgebraType  m g
+       , AlgebraType0 m g
+       , Eq (g a)
+       , Monad n
+       )
+    => proxy m
+    -> Gen (f a)
+    -> (f a -> String)
+    -> (forall x. f x -> g x)
+    -> PropertyT n ()
+foldNatFree_liftFree_property _ gen show_fa nat = do
+    (fa :: f a) <- H.forAllWith show_fa gen
+    H.assert $ (foldNatFree nat :: m f a -> g a) (liftFree fa :: m f a) == nat fa
+
+prop_foldNatFree_liftFree_coyoneda :: Property
+prop_foldNatFree_liftFree_coyoneda
+    = property $ foldNatFree_liftFree_property
+        (Proxy :: Proxy Coyoneda)
+        (Gen.maybe $ Gen.word8 (Range.linear 0 255))
+        show
+        (maybe (Left ()) Right)
+
+prop_foldNatFree_liftFree_ap :: Property
+prop_foldNatFree_liftFree_ap
+    = property $ foldNatFree_liftFree_property
+        (Proxy :: Proxy Ap)
+        (Gen.maybe $ Gen.word8 (Range.linear 0 255))
+        show
+        (maybe (Left ()) Right)
+
+prop_foldNatFree_liftFree_free :: Property
+prop_foldNatFree_liftFree_free
+    = property $ foldNatFree_liftFree_property
+        (Proxy :: Proxy Free)
+        (Gen.maybe $ Gen.word8 (Range.linear 0 255))
+        show
+        (maybe (Left ()) Right)
+
+foldNatFree_foldNatFree_property
+    :: forall m f g h a proxy n.
+       ( FreeAlgebra1 m
+       , AlgebraType0 m f
+       , AlgebraType0 m g
+       , AlgebraType  m g
+       , AlgebraType  m h
+       , Monad n
+       )
+    => proxy m
+    -> Gen (m f a)
+    -> (m f a -> String)
+    -> (h a -> h a -> Bool)
+    -> (forall x. g x -> h x)
+    -> (forall x. f x -> m g x)
+    -> PropertyT n ()
+foldNatFree_foldNatFree_property _ gen show_mfa eq_ha nat nat' =
+    case codom1 :: Proof (AlgebraType m (m g)) (m g) of
+      Proof -> do
+          (mfa :: m f a) <- H.forAllWith show_mfa gen
+          H.assert $ (foldNatFree nat :: m g a -> h a) (foldNatFree nat' mfa)
+                  `eq_ha` foldNatFree (foldNatFree nat . nat') mfa
+
+prop_foldNatFree_foldNatFree_coyoneda :: Property
+prop_foldNatFree_foldNatFree_coyoneda = property $ do
+    n <- H.forAll $ Gen.integral (Range.linear 0 255)
+    foldNatFree_foldNatFree_property
+      (Proxy :: Proxy Coyoneda)
+      (genCoyoneda toOdd :: Gen (Coyoneda Maybe Int))
+      show
+      (==)
+      (maybe (Left ()) Right)
+      (liftFreeCoyonedaN n)
+  where
+    liftFreeCoyonedaN :: Int -> Maybe x -> Coyoneda Maybe x
+    liftFreeCoyonedaN 0 fx = liftFree fx
+    liftFreeCoyonedaN n fx = id <$> liftFreeCoyonedaN (pred n) fx
+
+prop_foldNatFree_foldNatFree_ap :: Property
+prop_foldNatFree_foldNatFree_ap
+    = property $ do
+        n  <- H.forAll $ Gen.integral (Range.linear 0 255)
+        foldNatFree_foldNatFree_property
+          (Proxy :: Proxy Ap)
+          (genAp (Gen.integral (Range.linear 0 255)) genIntToInt :: Gen (Ap Maybe Int))
+          (const "blind")
+          (==)
+          (maybe (Left ()) Right)
+          (liftApN n id)
+  where
+    liftApN :: Int -> (x -> x) -> Maybe x -> Ap Maybe x
+    liftApN 0 _ ma = Ap.liftAp ma
+    liftApN n f ma = pure f <*> liftApN (pred n) f ma
+
+prop_foldNatFree_foldNatFree_free :: Property
+prop_foldNatFree_foldNatFree_free = property $ do
+    n <- H.forAll $ Gen.integral (Range.linear 0 255)
+    foldNatFree_foldNatFree_property
+      (Proxy :: Proxy Free)
+      (genFree (Gen.integral (Range.linear 0 255)) :: Gen (Free Maybe Int))
+      show
+      (==)
+      (maybe (Left ()) Right)
+      (liftFreeN n)
+  where
+    liftFreeN :: Int -> Maybe a -> Free Maybe a
+    liftFreeN 0 ma = liftFree ma
+    -- each bind wraps the lhs in a 'Free' constructor
+    liftFreeN n ma = liftFreeN (pred n) ma >>= liftFree . Just
+
+prop_foldNatFree_foldNatFree_StateT :: Property
+prop_foldNatFree_foldNatFree_StateT = property $ do
+    f1 <- H.forAll genIntToInt
+    f2 <- H.forAll genIntToInt
+    f3 <- H.forAll genIntToInt
+    foldNatFree_foldNatFree_property
+      (Proxy :: Proxy (S.StateT Int))
+      gen
+      (const "blind")
+      (\s s' -> S.runStateT s 0 == S.runStateT s' 0)
+      ((\s -> s >>= \x -> S.modify f1 >> pure x)
+        :: forall x. S.StateT Int Identity x -> S.StateT Int Identity x)
+      ((\(Identity x) -> S.lift (S.modify f2) >> S.modify f3 $> x)
+        :: forall x. Identity x -> S.StateT Int (S.StateT Int Identity) x)
+  where
+    gen :: Gen (S.StateT Int Identity Int)
+    gen = do
+      a <- Gen.int $ Range.linear 0 9
+      if a >= 3
+        then do
+          s <- Gen.int $ Range.linear 0 100
+          x <- Gen.int $ Range.linear 0 100
+          pure (S.put s $> x)
+        else pure S.get
 
 tests :: IO Bool
 tests = H.checkParallel $$(H.discover)
